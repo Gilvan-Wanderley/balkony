204bd9648ddcf3b1aea398d7e5592484fd5ab647
Gilvan Wanderley <gilvan.wanderley@outlook.com>
2024-09-13 19:10:50 -0300

    add test for centrifuge model

--- tests/capital_cost/test_centrifuges.py
+++ tests/capital_cost/test_centrifuges.py
@@ -1,31 +1,38 @@
 import pytest
-from balkony.capital_cost import BlenderCost
+from balkony.capital_cost import CentrifugeCost
 from utils import calculate_purchased, calculate_bare_module_simple
 
 TOLERANCE = 1e-6
 
 @pytest.fixture
-def kneader_blender():
-    return (BlenderCost(type=BlenderCost.Type.Kneader), 
-            (5.0141, 0.5867, 0.3224),
-            1.12)
+def apron_centrifuge():
+    return (CentrifugeCost(type=CentrifugeCost.Type.Apron), 
+            (3.9255, 0.5039, 0.1506),
+            1.20)
 
 @pytest.fixture
-def ribbon_blender():
-    return (BlenderCost(type=BlenderCost.Type.Ribbon), 
-            (4.1366, 0.5072, 0.0070),
-            1.12)
+def belt_centrifuge():
+    return (CentrifugeCost(type=CentrifugeCost.Type.Belt), 
+            (4.0637, 0.2584, 0.1550),
+            1.25)
 
 @pytest.fixture
-def rotary_blender():
-    return (BlenderCost(type=BlenderCost.Type.Rotary), 
-            (4.1366, 0.5072, 0.0070),
-            1.12)
+def pneumatic_centrifuge():
+    return (CentrifugeCost(type=CentrifugeCost.Type.Pneumatic), 
+            (4.6616, 0.3205, 0.0638),
+            1.25)
+
+@pytest.fixture
+def screw_centrifuge():
+    return (CentrifugeCost(type=CentrifugeCost.Type.Screw), 
+            (3.6062, 0.2659, 0.1982),
+            1.10)
 
 @pytest.mark.parametrize("fixture, size, CEPCI", [
-    ("kneader_blender", 1.0, 397),
-    ("ribbon_blender", 2.0, 397),
-    ("rotary_blender", 3.0, 397),
+    ("apron_centrifuge", 3.0, 397),
+    ("belt_centrifuge", 2.0, 397),
+    ("pneumatic_centrifuge", 3.0, 397),
+    ("screw_centrifuge", 4.0, 397),
 ])
 def test_within_limits(fixture, size, CEPCI, request):
     equipment, parameters, Fbm = request.getfixturevalue(fixture)
@@ -33,7 +40,7 @@ def test_within_limits(fixture, size, CEPCI, request):
     purchased_value = calculate_purchased(K1, K2, K3, size, CEPCI)
     bare_value = calculate_bare_module_simple(purchased_value, Fbm)
 
-    purchased =  equipment.purchased(size)
+    purchased = equipment.purchased(size)
     bare = equipment.bare_module(size)
 
     assert len(purchased.status) == 1
@@ -45,11 +52,11 @@ def test_within_limits(fixture, size, CEPCI, request):
     assert abs(purchased.value - purchased_value) < TOLERANCE
     assert abs(bare.value - bare_value) < TOLERANCE
 
-
 @pytest.mark.parametrize("fixture, size, CEPCI", [
-    ("kneader_blender", 0.1, 397),
-    ("ribbon_blender", 0.5, 397),
-    ("rotary_blender", 0.5, 397),
+    ("apron_centrifuge", 0.5, 397),
+    ("belt_centrifuge", 0.4, 397),
+    ("pneumatic_centrifuge", 0.6, 397),
+    ("screw_centrifuge", 0.3, 397),
 ])
 def test_below_limits(fixture, size, CEPCI, request):
     equipment, parameters, Fbm = request.getfixturevalue(fixture)
@@ -57,7 +64,7 @@ def test_below_limits(fixture, size, CEPCI, request):
     purchased_value = calculate_purchased(K1, K2, K3, size, CEPCI)
     bare_value = calculate_bare_module_simple(purchased_value, Fbm)
 
-    purchased =  equipment.purchased(size)
+    purchased = equipment.purchased(size)
     bare = equipment.bare_module(size)
 
     assert len(purchased.status) == 1
@@ -69,11 +76,11 @@ def test_below_limits(fixture, size, CEPCI, request):
     assert abs(purchased.value - purchased_value) < TOLERANCE
     assert abs(bare.value - bare_value) < TOLERANCE
 
-
 @pytest.mark.parametrize("fixture, size, CEPCI", [
-    ("kneader_blender", 5.0, 397),
-    ("ribbon_blender", 15.0, 397),
-    ("rotary_blender", 15.0, 397),
+    ("apron_centrifuge", 16.0, 397),
+    ("belt_centrifuge", 350.0, 397),
+    ("pneumatic_centrifuge", 70.0, 397),
+    ("screw_centrifuge", 31.0, 397),
 ])
 def test_above_limits(fixture, size, CEPCI, request):
     equipment, parameters, Fbm = request.getfixturevalue(fixture)
@@ -81,7 +88,7 @@ def test_above_limits(fixture, size, CEPCI, request):
     purchased_value = calculate_purchased(K1, K2, K3, size, CEPCI)
     bare_value = calculate_bare_module_simple(purchased_value, Fbm)
 
-    purchased =  equipment.purchased(size)
+    purchased = equipment.purchased(size)
     bare = equipment.bare_module(size)
 
     assert len(purchased.status) == 1
